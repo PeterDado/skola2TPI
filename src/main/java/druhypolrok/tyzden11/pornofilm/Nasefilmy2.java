b7b087c12e4e630b83fca7fc48c428f1009d3888
Dado <petkodado2005@gmail.com>
2022-04-12 17:26:34 +0200

    hi

--- src/main/java/druhypolrok/tyzden11/pornofilm/Nasefilmy2.java
+++ src/main/java/druhypolrok/tyzden11/pornofilm/Nasefilmy2.java
@@ -10,8 +10,12 @@ public class Nasefilmy2 {
     public static void main(String[] args) {
 
         PornoHerecka pornoHerecka = pornoHerecky().get(0);
+        List<PornoHerec> herci = pornoHerci();
+        herci.forEach(ph -> System.out.println(ph.getObjemVacku()));
 
+        pornoHerecka.robiGangbang(herci);
 
+        herci.forEach(ph -> System.out.println(ph.getObjemVacku()));
         getObjemVacku();
 
         pornoHerecka.robiGangbang(pornoHerci());
